0c951a4026f5b58c710a1e7b9c97b4b8ce78d4c0
domagoj-zdero <domagoj.zdero@gmail.com>
2018-03-05 21:09:05 +0100

    povezivanje php i html

---
+++ public/dzdero/2.5.povezivanje_html_php.php
@@ -0,0 +1,26 @@
+
+ <!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+<div>sadržaj neki</div>
+<div style="color:red"<?php echo "neki moj text iz PHP-a"."<br>";
+$a=6;
+$b=4;
+echo "rezultat zbroja je: ".($a+$b);
+?></div>
+</body>
+</html> 
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
