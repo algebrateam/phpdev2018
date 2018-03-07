93812d152d9b05624ce643b9ff1faf1516222bff
kovacevicilija <ilijak9@gmail.com>
2018-03-07 20:08:16 +0100

    poljavrijednosti

---
+++ public/ikovacevic/325polja.php
@@ -0,0 +1,46 @@
+<?php 
+
+$c =10;
+$d =20;
+$f =20;
+
+echo "vrijednost varijable c: ". $c;
+echo "<br>";
+echo "vrijednost varijable d: ". $d;
+echo "<br>";
+echo "vrijednost varijable f: ". $f;
+
+echo "<br>";
+
+$a =3;
+$b =5;
+
+$pom = $a;
+$a = $b;
+$b = $pom;
+
+echo "<br>";
+echo "izmjena  a i b: ". $a;
+echo "<br>";
+
+echo "<br>";
+echo "izmjena b i a: ". $b;
+
+
+echo "<br>";
+echo "<br>";
+$ime= "Ilija";
+$prezime="Kovacevic";
+
+echo $ime;
+echo ' ';
+echo $prezime;
+
+
+?>
+
+
+      
+
+
+
