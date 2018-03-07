b09e72899c9cd638e726788c356a50898e7a034d
BouliOS <danijel.krizanovic.com>
2018-03-08 00:00:54 +0100

    Zadaca2

---
+++ public/dkrizanovic/Zadace/Zadaca2.php
@@ -0,0 +1,62 @@
+<?php
+// Zadatak 1
+echo '<br>';
+echo 'Zadatak 1:';
+echo '<br>';
+echo '<br>';
+echo "Rjesenje prvog zadatka je pod slovom (a)";
+echo '<br>';
+echo '<br>';
+// Objašnjenje: Modul (%) je artimetički operator kojeg koristimo u PHP-u.
+//Rezultat modula jest ostatak djeljenja dvaju operanada. 
+echo "<hr>";
+
+//Zadatak 2
+echo '<br>';
+echo 'Zadatak 2:';
+echo '<br>';
+echo '<br>';
+$ime= "Danijel";
+$prezime="Krizanovic";
+echo "Moje ime je: $ime";
+echo "<br>";
+echo "Moje prezime je: $prezime";
+echo "<br>";
+echo 'Moje puno ime i prezime je: '.$ime.' '.$prezime ;
+echo '<br>';
+echo '<br>';
+// Objašnjenje: Operator za nastavljanje nizova je: (.);
+echo '<br>';
+echo '<br>';
+echo "<hr>";
+
+//Zadatak 3
+echo '<br>';
+echo "Zadatak 3 ";
+echo '<br>';
+echo '<br>';
+$a=5;
+$a++;
+echo "Varijabla a ima početnu vrijednost 5.";
+echo "<br>";
+echo "Varijabla a nakon automatskog povećavanja ima vrijednost $a.";
+echo "<br>";
+$a--;
+echo "Varijabla a nakon automatskog smanjivanja je $a.";
+echo "<br>";
+echo "<br>";
+// Objašnjenje: Operater za automatsko povećavanje je $var++ a za smanjivanje $var--
+echo "<hr>";
+
+//Zadatak 4
+echo "<br>";
+echo "Zadatak 4:";
+echo "<br>";
+echo "<br>";
+$a= (float)$a;
+echo $a;
+echo "<br>";
+echo "<br>";
+
+
+
