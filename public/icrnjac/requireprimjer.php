a02a1edfe738ddcb96e23371c2f64c58450642d1
ivana871 <misscrnjac@gmail.com>
2018-03-07 19:22:05 +0100

    novo

---
+++ public/icrnjac/requireprimjer.php
@@ -0,0 +1,12 @@
+<?php
+require "hello world"
+$tvar=0;
+require "hello php";
+include "dummy.php";
+echo
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
