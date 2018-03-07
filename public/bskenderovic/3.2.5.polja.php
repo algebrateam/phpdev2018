21a472591cfe23c63f4de9b721e7d8430a6089a5
belasken <bela.skenderovic@hdit.hr>
2018-03-07 19:53:13 +0100

    zadaci 3.3

---
+++ public/bskenderovic/3.2.5.polja.php
@@ -0,0 +1,30 @@
+<?php
+
+$polje = array('Tesla','Edison','Bell');
+echo $polje[0];
+echo $polje[1];
+echo $polje[2];
+echo "<br>";
+
+$r='35';
+echo $r;
+echo "<br>";
+
+$z='4,5';
+echo $z;
+echo "<br>";
+
+$ime = 'Bela Skenderović';
+echo $ime;
+echo "<br>";
+
+$a='3';
+$b='5';
+$pom=$a;
+$a=$b;
+$b=$pom;
+
+echo $a;
+echo $b;
+
+echo "<br>";
