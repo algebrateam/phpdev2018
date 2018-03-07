8b5aaa70549f5019cb11170cd7a6c68d902705e9
domagoj-zdero <domagoj.zdero@gmail.com>
2018-03-07 21:51:18 +0100

    vjezbe 4.2

---
+++ public/dzdero/4.2.pregledi-operatora.php
@@ -0,0 +1,32 @@
+<?php
+echo 2+3*4;
+echo "<br>";
+echo (2*3+4)."<br>";
+echo (2+3)*4;
+
+echo "<hr>";
+echo '1%1=';
+echo 1%1;
+echo "<br>";
+echo '7%5=';
+echo 7%5;
+echo "<br>";
+echo '37%4=';
+echo 37%4;
+
+echo "<hr>";
+
+$brojevi=[1,2,3,4,5,6,7,8,9,10];
+foreach ($brojevi as $value) {
+    if($value%2==0) {
+        echo $value." je paran broj <br>";
+        }
+    else {$value."je neparan broj <br>";
+    }
+    
+}
+
+
+
+
+?>
