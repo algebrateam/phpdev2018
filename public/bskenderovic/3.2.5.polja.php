3bc77134fac1ff40a22781dbe12f71b3a570acc6
belasken <bela.skenderovic@hdit.hr>
2018-03-07 20:59:17 +0100

    zadnji vježba

--- public/bskenderovic/3.2.5.polja.php
+++ public/bskenderovic/3.2.5.polja.php
@@ -1,26 +1,26 @@
 <?php
 
 $polje = array('Tesla','Edison','Bell');
-echo $polje[0];
-echo $polje[1];
-echo $polje[2];
+echo $polje[0]."<br>";
+echo $polje[1]."<br>";
+echo $polje[2]."<br>";
 echo "<br>";
 
-$r='35';
+$r=35;
 echo $r;
 echo "<br>";
 
-$z='4,5';
+$z=4.5;
 echo $z;
 echo "<br>";
 
-$a='3';
-$b='5';
+$a=3;
+$b=5;
 $pom=$a;
 $a=$b;
 $b=$pom;
 
-echo $a;
+echo $a."<br>";
 echo $b;
 
 echo "<br>";
