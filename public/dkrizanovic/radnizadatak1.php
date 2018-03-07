a7ab441132f8b306378f93aef9357fa7b82c321d
dkrizanovic <danijel.krizanovic@gmail.com>
2018-03-07 20:36:48 +0100

    ispravak

--- public/dkrizanovic/radnizadatak1.php
+++ public/dkrizanovic/radnizadatak1.php
@@ -1,16 +1,16 @@
 <?php
 //zadatak 1
-$varijabla1=" 0001";
-$varijabla2=" 0002";
-$varijabla3=" 0003";
+$varijabla1='0001';
+$varijabla2=' 0002';
+$varijabla3=' 0003';
 
 echo $varijabla1, $varijabla2,$varijabla3;
 
 
 //zadatak 2
 echo "</br>";
-$a="3";
-$b="5";
+$a='3';
+$b='5';
 $pom=$a;
 $a=$b;
 $b=$pom;
@@ -22,9 +22,9 @@ echo "</br>";
 
 
 //zadatak 3
-$v4="Danijel";
-$v5=" Krizanovic";
-echo $v4, $v5;
+$neki_text='Danijel Krizanovic';
+echo 'Ovo je neki probni tekst:'.$neki_text.'<br>';
+
 
 
         
