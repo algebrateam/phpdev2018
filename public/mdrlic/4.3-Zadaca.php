ebc0b2e92ece3d8cc8aad430331e32101c852c87
mdrlic <margareta.drlic@gmail.com>
2018-03-09 14:14:28 +0100

    Zadaća

---
+++ public/mdrlic/4.3-Zadaca.php
@@ -0,0 +1,53 @@
+<?php
+echo "Zadatak broj 1<br>Aritmetički operator od navedenih operatora je pod a) %."."<br>"."<br>";
+
+echo "Zadatak broj 2<br>";
+$ime = "Margareta";
+$prezime = "Drlić";
+echo $ime." ".$prezime."<br>"."<br>";
+
+echo "Zadatak broj 3<br>";
+$a = 5;
+$a++;
+echo $a."<br>";
+
+$a = 5;
+$a--;
+echo $a."<br>"."<br>";
+
+echo "Zadatak broj 4<br>";
+$a = 5;
+$a = (float)$a;
+echo $a."<br>"."<br>";
+
+echo "Zadatak broj 5<br>";
+$a = 5;
+$a += 5;
+echo $a."<br>"."<br>";
+
+echo "Zadatak broj 6<br>";
+$a = 7;
+$b = 3;
+echo "a. ".$a." + ".$b." = ".($a+$b)."<br>";
+echo "b. ".$a." - ".$b." = ".($a-$b)."<br>";
+echo "c. ".$a." * ".$b." = ".($a*$b)."<br>";
+echo "d. ".$a." / ".$b." = ".($a/$b)."<br>";
+echo "e. ".$a." % ".$b." = ".($a%$b)."<br>"."<br>";
+
+echo "Zadatak broj 7<br>";
+$a = 4;
+$b = 2;
+echo "a. ".$a." + ".$b." = ".($a+$b)."<br>";
+echo "b. ".$a." - ".$b." = ".($a-$b)."<br>";
+echo "c. ".$a." * ".$b." = ".($a*$b)."<br>";
+echo "d. ".$a." / ".$b." = ".($a/$b)."<br>";
+echo "e. ".$a." % ".$b." = ".($a%$b)."<br>";
+
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
