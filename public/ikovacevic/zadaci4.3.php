e4af7912b3e6719506cb1753e5a1f670eb305d74
kovacevicilija <ilijak9@gmail.com>
2018-03-10 19:18:09 +0100

    promjena

--- public/ikovacevic/zadaci4.3.php
+++ public/ikovacevic/zadaci4.3.php
@@ -16,12 +16,11 @@ echo "Zadatak drugi ispisi ime i prezime";
 echo "<br>";echo "<br>";
 
 
-         $mojeime="Ilija";
-         $prezime="Kovacevic";
+$mojeime="Ilija"; $prezime="Kovacevic";
          
-         echo $mojeime." ".$prezime;
+echo $mojeime." ".$prezime;
             
-         echo "<br>";echo "<br>";
+echo "<br>";echo "<br>";
          
          
          
@@ -29,46 +28,45 @@ echo "Zadatak treci";
          
 echo "<br>";echo "<br>";
 
-
-
-         $a=5;
-         $a++;
+$a=5; $a++;
          
-         echo $a;
+echo $a; $a--;
+echo $a;
          
-         $a--;
-         echo $a;
-         
-         echo "<br>";echo "<br>";
+echo "<br>";echo "<br>";
          
          
-         echo "Zadatak cetvrti";
+echo "Zadatak cetvrti";
          
-         echo "<br>";echo "<br>";
+echo "<br>";echo "<br>";
          
          
-         $b=5;
-         $b=(float)$b;
-         echo $b;
+$b=5;
+$b=(float)$b;
+echo $b;
          
-         echo "<br>";echo "<br>";
+echo "<br>";echo "<br>";
         
          
 echo "Zadatak peti";
 
-$c=5;
-$c+=5;
+$c=5;  $c+=5;
 echo "<br>";echo "<br>";
 echo $c;
-         
-      $d=7;
-      $e=3;
-      echo $d." + ".$e." = ".($d+&e)."<br/>";
-           
-         
-         
-         
 
-?>
+echo "<br>"; echo "<br>";
+echo "Zadatak sesti";
+         
+     $d=7; $e=3;
+      
+      echo "<br>"; echo "<br>";
+      
+      echo $d." + ".$e." = ".($d+$e)."<br/>" ; 
+       echo $d." - ".$e." = ".($d-$e)."<br/>" ;   
+       echo $d." * ".$e." = ".($d*$e)."<br/>" ;   
+       echo $d." / ".$e." = ".($d/$e)."<br/>"  ;  
+       echo $d." % ".$e." = ".($d%$e)."<br/>"  ;
+
+              ?>
 
 
