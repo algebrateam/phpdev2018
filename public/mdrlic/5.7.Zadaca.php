8cab58700556b5f2820b12c81bdd3d13484466a2
mdrlic <margareta.drlic@gmail.com>
2018-03-14 11:34:51 +0100

    Zadaća if-else

---
+++ public/mdrlic/5.7.Zadaca.php
@@ -0,0 +1,36 @@
+<?php
+
+echo 'Zadatak broj 1<br>';
+$a = 4;
+$b = 2;
+
+if($a > $b){
+    echo $a.' je veći od '.$b.'<br><br>';
+}
+
+if($a < $b){
+    echo $a.' je manji od '.$b.'<br><br>';
+}
+
+echo 'Zadatak broj 2<br>';
+$a = 5;
+$b = 10;
+
+if($a > $b){
+    $a++;
+}
+else{
+    $a--;
+}
+
+echo 'Varijabla a ima vrijednost '.$a.'<br>Varijabla b ima vrijednost '.$b.'<br><br>';
+
+
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
