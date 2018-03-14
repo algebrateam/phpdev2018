2bd3f034241eccc06e494b0d8f0b07f28468517f
mdrlic <margareta.drlic@gmail.com>
2018-03-14 12:47:11 +0100

    petlja while

--- public/mdrlic/5.7.Zadaca.php
+++ public/mdrlic/5.7.Zadaca.php
@@ -25,9 +25,15 @@ else{
 
 echo 'Varijabla a ima vrijednost '.$a.'<br>Varijabla b ima vrijednost '.$b.'<br><br>';
 
+echo 'Zadatak broj 3<br>';
+$i = 1;
 
+while($i <= 50){
+    echo $i.'<br>';
+    $i++;
+}
 
-
+        
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
