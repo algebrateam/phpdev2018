44fd9ea7958c7ec26ada26c59c4fb04683095bbd
ivranjes <ivan.vranjes@t.ht.hr>
2018-03-15 21:12:03 +0100

    vjezbe polja

---
+++ public/ivranjes/6.3.1.1.php
@@ -0,0 +1,10 @@
+<?php
+
+$polje = array('Tesla','Edison','Bell');
+
+foreach($polje as $ime)
+{
+    echo $ime;
+}
+
+
---
+++ public/ivranjes/6.5.1.php
@@ -0,0 +1,8 @@
+<?php
+
+$fruits = array(0=>'konj','a' =>'naranca',1 =>'banana','b' =>'jabuka','c' =>'zec');
+echo '<pre>';
+sort($fruits);
+
+print_r($fruits);
+
