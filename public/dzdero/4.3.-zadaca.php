436d429cef79d52bd5adff621d3f233cce4f4daa
Domagoj Ždero <domagoj.zdero@ured.uduosk>
2018-03-15 08:25:44 +0100

    update zadaca 4.3.

--- public/dzdero/4.3.-zadaca.php
+++ public/dzdero/4.3.-zadaca.php
@@ -1,13 +1,13 @@
 <?php
 
 //zadatak 1. strana 39
-echo '1. Od navedenih operatora aritmetički operator je pod a. %'.'<br>'.'<br>';
+echo '1. Od navedenih operatora aritmetički operator je pod a. %'.'<br>'.'<hr>'.'<br>';
 
 //zadatak 2. strana 39
 echo '2. ';
 $ime = 'Domagoj';
 $prezime = 'Ždero';
-echo "$ime"." $prezime".'<br>'.'<br>';
+echo "$ime"." $prezime".'<br>'.'<hr>'.'<br>';
 
 //zadatak 3. strana 39
 echo '3. ';
@@ -18,38 +18,40 @@ $a = 5;
 $a = --$a;
 
 echo "$a";
-echo '<br>'.'<br>';
+echo '<br>'.'<hr>'.'<br>';
 
 //zadatak 4. strana 39
 echo '4. ';
 $a = 5;
 $a = (float) $a;
 echo $a;
-echo '<br>'.'<br>';
+echo '<br>'.'<hr>'.'<br>';
 
 //zadatak 5. strana 39
 echo '5. ';
 $a += 5;
 echo $a;
-echo '<br>'.'<br>';
+echo '<br>'.'<hr>'.'<br>';
 
 //zadatak 6. strana 39
 echo '6. ';
 $a = 7;
 $b = 3;
-echo 'a. '.($a + $b).', ';
-echo 'b. '.($a - $b).', ';
-echo 'c. '.($a * $b).', ';
-echo 'd. '.($a / $b).', ';
-echo 'e. '.($a % $b);
-echo '<br>'.'<br>';
+echo '$a'.' je '."$a".';&nbsp'.'$b'.' je '."$b".'<br>';
+echo '&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'a. '."$a + $b".' = '.($a + $b).'<br>';
+echo '&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'b. '."$a - $b".' = '.($a - $b).'<br>';
+echo '&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'c. '."$a * $b".' = '.($a * $b).'<br>';
+echo '&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'d. '."$a / $b".' = '.($a / $b).'<br>';
+echo '&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'e. '."$a % $b".' = '.($a % $b);
+echo '<br>'.'<hr>'.'<br>';
 
 //zadatak 7. strana 39
 echo '7. ';
 $a = 4;
 $b = 2;
-echo 'a. '.($a + $b).', ';
-echo 'b. '.($a - $b).', ';
-echo 'c. '.($a * $b).', ';
-echo 'd. '.($a / $b).', ';
-echo 'e. '.($a % $b);
+echo '$a'.' je '."$a".';&nbsp'.'$b'.' je '."$b".'<br>';
+echo '&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'a. '."$a + $b".' = '.($a + $b).'<br>';
+echo '&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'b. '."$a - $b".' = '.($a - $b).'<br>';
+echo '&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'c. '."$a * $b".' = '.($a * $b).'<br>';
+echo '&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'d. '."$a / $b".' = '.($a / $b).'<br>';
+echo '&nbsp'.'&nbsp'.'&nbsp'.'&nbsp'.'e. '."$a % $b".' = '.($a % $b);
