f4ffad1d88f317647ea60e681850e900791f774b
ivranjes <ivan.vranjes@t.ht.hr>
2018-03-15 21:55:30 +0100

    vježbe

--- public/ivranjes/6.5.1.php
+++ public/ivranjes/6.5.1.php
@@ -3,6 +3,7 @@
 $fruits = array(0=>'konj','a' =>'naranca',1 =>'banana','b' =>'jabuka','c' =>'zec');
 echo '<pre>';
 sort($fruits);
+echo '<br>prebrojali smo '. count($fruits);
 
 print_r($fruits);
 
