3cbaf26a94ea1fa9ee71f30ede452b0722cbeeb4
drepinac <darko.repinac@udu-obz.hr>
2018-03-15 20:15:57 +0100

    6.4.1

--- public/drepinac/Vjezba 6.3.php
+++ public/drepinac/Vjezba 6.3.php
@@ -75,6 +75,16 @@ foreach($ucenici as $br_ucenika => $podaci)
         }
 }
 
+echo '<hr>';
+$elem1 = array(1,5,7);
+$elem2 = array(4,8,9);
+$elem3 = array(2,3,6);
+
+$multi_array = array($elem1, $elem2, $elem3);
+
+echo '<pre>';
+print_r($multi_array);
+echo '</pre>';
 ?>
 
 </body>
