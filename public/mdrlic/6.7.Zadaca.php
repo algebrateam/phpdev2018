5318c2afa6292129d0027a392a8610caf69eda3b
mdrlic <margareta.drlic@gmail.com>
2018-03-16 12:21:59 +0100

    Promjena vrijednosti ključa

--- public/mdrlic/6.7.Zadaca.php
+++ public/mdrlic/6.7.Zadaca.php
@@ -8,7 +8,18 @@ foreach($polje_imena as $ime)
     echo $ime.'<br>';
 }
 
+echo '<br>Zadatak broj 2<br>';
+$polje_imena = array();
+$polje_imena['ime5'] = 'Martina';
+$polje_imena['ime4'] = 'Matea';
+$polje_imena['ime3'] = 'Lea';
+$polje_imena['ime2'] = 'Marijan';
+$polje_imena['ime1'] = 'Leon';
 
+foreach($polje_imena as $key => $ime)
+{
+    echo 'Ključ '.$key.' ima vrijednost: '.$ime.'<br>';
+}
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
