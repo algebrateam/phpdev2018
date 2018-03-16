4f421df0d06300d0869b1d1104976553dd229f0f
mdrlic <margareta.drlic@gmail.com>
2018-03-16 12:10:13 +0100

    Polje i petlja foreach

---
+++ public/mdrlic/6.7.Zadaca.php
@@ -0,0 +1,17 @@
+<?php
+
+echo 'Zadatak broj 1<br>';
+$polje_imena = array('Martina', 'Matea', 'Lea', 'Marijan', 'Leon');
+
+foreach($polje_imena as $ime)
+{
+    echo $ime.'<br>';
+}
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
