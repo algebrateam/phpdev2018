398018710e9e8bbf0b7a82e1e179e2908f8e5f23
dkrizanovic <danijel.krizanovic@gmail.com>
2018-03-19 20:21:45 +0100

    uvod u funkcije

---
+++ public/dkrizanovic/Predavanja/7.1.uvod_u_funkcije.php
@@ -0,0 +1,27 @@
+<?php
+
+function show_naslov($naslov = 'no_title')
+{
+    switch($naslov):
+        case 'film':
+           $var = 'Terminator';
+            break;
+        case 'serija':
+            $var = 'Dr. House';
+            break;
+        case 'no_title':
+            $var = "";
+            break;
+      
+    endswitch;
+    echo $var;
+    }
+    function show_imena($imena = array())
+    {
+        foreach($imena as $ime)
+        {
+            echo $ime;
+            }
+    }
+    show_naslov('film');
+    show_imena(('Bela', 'Predrag''));
