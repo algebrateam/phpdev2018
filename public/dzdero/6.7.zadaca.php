06819e7d8d8b33fcafa6ae6e4ef8d032c05303d0
Danijel Krizanovic <danijel.krizanovic@gmail.com>
2018-03-19 20:33:04 +0000

    Apply fixes from StyleCI

--- public/dzdero/6.7.zadaca.php
+++ public/dzdero/6.7.zadaca.php
@@ -6,13 +6,13 @@ foreach ($imena as $value) {
     echo $value.'<br>';
 }
 echo '<pre>';
-print_r ($imena);
+print_r($imena);
 echo '</pre>';
 echo '<hr><br>';
 echo 'Zadatak 2.<br>';
 $imena=['ime5'=>'Ivan','ime4'=>'Adam','ime3'=>'Ferdinand','ime2'=>'Bruno','ime1'=>'Gertruda',];
 echo '<pre>';
-print_r ($imena);
+print_r($imena);
 echo '</pre>';
 
 echo '<hr><br>';
@@ -20,11 +20,11 @@ echo 'Zadatak 3.<br>';
 //$imena=['Ivan','Adam','Ferdinand','Bruno','Gertruda'];
 sort($imena);
 echo '<pre>';
-print_r ($imena);
+print_r($imena);
 echo '</pre>';
  
 
-for ($i = 0; $i < count($imena); $i++ ){
+for ($i = 0; $i < count($imena); $i++) {
     //sort($imena);
     echo $imena[$i].'<br>';
 }
@@ -33,8 +33,8 @@ echo '<hr><br>';
 echo 'Zadatak 4.<br>';
 ksort($imena);
 foreach ($imena as $key => $ime) {
-            echo 'ključ '.$key.' uz ime '.$ime.'</br>';
-        }
+    echo 'ključ '.$key.' uz ime '.$ime.'</br>';
+}
 
 
 echo '<hr><br>';
@@ -42,8 +42,7 @@ echo 'Zadatak 5.<br>';
 $niz=array();
 for ($brojevi=1; $brojevi<=100; $brojevi++) {
     $niz[]=$brojevi;
-    
 }
 foreach ($niz as $broj) {
-            echo $broj.'</br>';
+    echo $broj.'</br>';
 }
