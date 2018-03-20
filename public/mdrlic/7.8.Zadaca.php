0b8c5d52a4939443bf9f73819620c0df5b07970e
mdrlic <margareta.drlic@gmail.com>
2018-03-20 10:56:05 +0100

    Funkcija date('n')

---
+++ public/mdrlic/7.8.Zadaca.php
@@ -0,0 +1,60 @@
+<?php
+
+echo 'Zadatak 1<br>';
+function hr_month()
+{
+    switch (date('n')):
+        
+        case '1':
+            $month = 'siječanj';
+            break;
+        case '2':
+            $month = 'veljača';
+            break;
+        case '3':
+            $month = 'ožujak';
+            break;
+        case '4':
+            $month = 'travanj';
+            break;
+        case '5':
+            $month = 'svibanj';
+            break;
+        case '6':
+            $month = 'lipanj';
+            break;
+        case '7':
+            $month = 'srpanj';
+            break;
+        case '8':
+            $month = 'kolovoz';
+            break;
+        case '9':
+            $month = 'rujan';
+            break;
+        case '10':
+            $month = 'listopad';
+            break;
+        case '11':
+            $month = 'studeni';
+            break;
+        case '12':
+            $month = 'prosinac';
+            break;
+          
+    endswitch;
+    
+    echo 'Ovaj mjesec je: '.$month;
+}
+
+hr_month();
+
+
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
