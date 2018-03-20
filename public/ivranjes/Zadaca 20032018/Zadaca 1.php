45a3f05b6f71ac4c24f7161b7fdb1722a176650a
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-03-20 13:54:40 +0000

    Apply fixes from StyleCI

--- public/ivranjes/Zadaca 20032018/Zadaca 1.php
+++ public/ivranjes/Zadaca 20032018/Zadaca 1.php
@@ -2,21 +2,21 @@
 
 function hr_mjesec($mj)
 {
-	$mj_array = array();
-	$mj_array[1] = "Sijeèanj";
-	$mj_array[2] = "Veljaèa";
-	$mj_array[3] = "Oujak";
-	$mj_array[4] = "Travanj";
-	$mj_array[5] = "Svibanj";
-	$mj_array[6] = "Lipanj";
-	$mj_array[7] = "Srpanj";
-	$mj_array[8] = "Kolovoz";
-	$mj_array[9] = "Rujan";
-	$mj_array[10] = "Listopad";
-	$mj_array[11] = "Studeni";
-	$mj_array[12] = "Prosinac";
-	
-	return $mj_array[$mj];
+    $mj_array = array();
+    $mj_array[1] = "Sijeèanj";
+    $mj_array[2] = "Veljaèa";
+    $mj_array[3] = "Oujak";
+    $mj_array[4] = "Travanj";
+    $mj_array[5] = "Svibanj";
+    $mj_array[6] = "Lipanj";
+    $mj_array[7] = "Srpanj";
+    $mj_array[8] = "Kolovoz";
+    $mj_array[9] = "Rujan";
+    $mj_array[10] = "Listopad";
+    $mj_array[11] = "Studeni";
+    $mj_array[12] = "Prosinac";
+    
+    return $mj_array[$mj];
 }
 
 $mj = date('n');
--- public/ivranjes/Zadaca 20032018/Zadaca 2.php
+++ public/ivranjes/Zadaca 20032018/Zadaca 2.php
@@ -3,19 +3,18 @@
 
 $naziv = "Algebrao";
 
-switch($naziv)
-{
-	case 'Algebra';
-	echo 'To je full ok';
-		break;
-		
-		case 'Nonalgebra';
-		echo 'To nije to nikako';
-	break;
-	
-	default;
-		
-	echo "tumpav sam ja";
-		break;
-	
+switch ($naziv) {
+    case 'Algebra':
+    echo 'To je full ok';
+        break;
+        
+        case 'Nonalgebra':
+        echo 'To nije to nikako';
+    break;
+    
+    default:
+        
+    echo "tumpav sam ja";
+        break;
+    
 }
