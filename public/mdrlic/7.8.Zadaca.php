e841947bd61bc1f928317df640487a67ade42c64
mdrlic <margareta.drlic@gmail.com>
2018-03-20 10:58:13 +0100

    Funkcija ispis tablice

--- public/mdrlic/7.8.Zadaca.php
+++ public/mdrlic/7.8.Zadaca.php
@@ -50,6 +50,41 @@ function hr_month()
 hr_month();
 
 
+echo '<br><br>Zadatak 2<br>';
+function ispis_tablice($imena)
+{
+    echo '
+    <table border = 1>
+    <tr>
+    <th>Redni broj</th>
+    <th>Ime</th>
+    <th>Prezime</th>
+    </tr>';
+    
+    foreach($imena as $key => $ucenik)
+    {
+        echo '
+        <tr>
+        <td>'.$key.'</td>
+        <td>'.$ucenik['ime'].'</td>
+        <td>'.$ucenik['prezime'].'</td>
+        </tr>';
+    }
+    echo '</table>';
+}
+
+$ucenici = array();
+
+$ucenici[1]['ime'] = 'Iva';
+$ucenici[1]['prezime'] = 'Matić';
+
+$ucenici[2]['ime'] = 'Katarina';
+$ucenici[2]['prezime'] = 'Krivić';
+
+$ucenici[3]['ime'] = 'Matea';
+$ucenici[3]['prezime'] = 'Starčević';
+
+ispis_tablice($ucenici);
 
 
 /* 
