b6924f199394a48573f218b905d247d48478e978
pmrvic <mrvic.predrag@gmail.com>
2018-03-21 22:42:41 +0100

    dodana help stranica algebrateam

---
+++ public/pmrvic/algebra2017-phpTeam/config/configuration.php
@@ -0,0 +1,37 @@
+<?php
+
+$title = 'Algebra PHP i MySQL 2017';
+
+
+//* MENU LINKS*//
+$home = '../';
+$github = 'https://github.com/';
+$trello = 'https://trello.com/';
+$netbeans = 'https://netbeans.org/';
+$xampp= 'https://www.apachefriends.org/index.html';
+$heidisql= 'https://www.heidisql.com/';
+
+//* LISTA KORISNIKA *//
+$pmrvic= 'https://github.com/mrvic';
+$bvidovic= 'https://github.com/bvidov45';
+$dcuric= 'https://github.com/domagojcuric';
+$dudovcic= 'https://github.com/dudovcic';
+$dvalencak= 'https://github.com/Domac13';
+$gtolusic= 'https://github.com/Gorant696';
+$imazur= 'https://github.com/ivanmazur001';
+$ipaic= 'https://github.com/iPaich';
+$zilicic= 'https://github.com/zilicic';
+$ndragun= 'https://github.com/NemanjaDragun92Git';
+
+//* FOOTER LINKS *//
+$gitbash = 'https://git-scm.com/downloads';
+$w3schools = 'https://www.w3schools.com/';
+
+//* COPYRIGHT LINK *//
+$copyright = 'https://nemanjadragun.com/';
+
+//* COMPLETED / NON-COMPLETED TASKS (img/yes.png = completed // img/no.png = non-completed) *//
+$completed = 'img/yes.png';
+$notcompleted = 'img/no.png';
+
+?>
