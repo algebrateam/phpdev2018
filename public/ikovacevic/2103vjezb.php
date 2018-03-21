0a44e5e54564fac291272cf1b4324bb1a3c80bd1
kovacevicilija <ilijak9@gmail.com>
2018-03-21 22:29:30 +0100

    objekti vjezba

---
+++ public/ikovacevic/2103vjezb.php
@@ -0,0 +1,29 @@
+<?php
+
+class Covjek
+{
+    public $ime;
+    
+    function set_ime($novo_ime)
+    {
+        $this->ime=$novo_ime;
+        
+    }
+    
+    function get_ime()
+    {
+        return $this->ime;
+    }
+  
+}
+
+$c=new Covjek();
+$c->set_ime('Ivana Brlic Mazuranic');
+echo $c->ime;
+// veliko prvo slovo 
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
