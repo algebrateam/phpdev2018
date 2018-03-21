ea4e410b113b8e41fa7763128a9ca609b1f057e7
pmrvic <mrvic.predrag@gmail.com>
2018-03-21 22:35:34 +0100

    Predavanja objekti

---
+++ public/pmrvic/37-40/Covjek.php
@@ -0,0 +1,24 @@
+<?php
+class Covjek{
+  public function __construct() {  // DEFAULTNA METODA KREIRANJA
+    echo "Rodio se jedan covjek<br>";
+  }
+  public function __destruct() { // DEFAULTNA METODA KREIRANJA
+    echo "<br> XXX Kaput<br>";
+  }
+  
+  private $ime;
+  public function set_ime($novo_ime){
+    $this->ime=ucfirst($novo_ime);  //Mutator
+    
+  } 
+  public function get_ime() {
+    $samo_ime=explode(' ',$this->ime);
+    return $samo_ime[0];      // Mutator
+  }
+    public function get_prezime() {
+    $samo_ime=explode(' ',$this->ime);
+    return $samo_ime[2];      // Mutator
+  }
+}
+
