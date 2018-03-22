f4c3602935327bbce148b93a74c81425103ad8e8
Danijel Krizanovic <danijel.krizanovic@gmail.com>
2018-03-22 12:20:28 +0000

    Apply fixes from StyleCI

--- public/pmrvic/37-40/Covjek.php
+++ public/pmrvic/37-40/Covjek.php
@@ -1,24 +1,28 @@
 <?php
-class Covjek{
-  public function __construct() {  // DEFAULTNA METODA KREIRANJA
-    echo "Rodio se jedan covjek<br>";
-  }
-  public function __destruct() { // DEFAULTNA METODA KREIRANJA
-    echo "<br> XXX Kaput<br>";
-  }
+class Covjek
+{
+    public function __construct()
+    {  // DEFAULTNA METODA KREIRANJA
+        echo "Rodio se jedan covjek<br>";
+    }
+    public function __destruct()
+    { // DEFAULTNA METODA KREIRANJA
+        echo "<br> XXX Kaput<br>";
+    }
   
-  private $ime;
-  public function set_ime($novo_ime){
-    $this->ime=ucfirst($novo_ime);  //Mutator
-    
-  } 
-  public function get_ime() {
-    $samo_ime=explode(' ',$this->ime);
-    return $samo_ime[0];      // Mutator
-  }
-    public function get_prezime() {
-    $samo_ime=explode(' ',$this->ime);
-    return $samo_ime[2];      // Mutator
-  }
+    private $ime;
+    public function set_ime($novo_ime)
+    {
+        $this->ime=ucfirst($novo_ime);  //Mutator
+    }
+    public function get_ime()
+    {
+        $samo_ime=explode(' ', $this->ime);
+        return $samo_ime[0];      // Mutator
+    }
+    public function get_prezime()
+    {
+        $samo_ime=explode(' ', $this->ime);
+        return $samo_ime[2];      // Mutator
+    }
 }
-
