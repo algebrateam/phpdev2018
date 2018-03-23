3043e1d54c302aec47324c80d839c72ce095833c
Domagoj Ždero <domagoj.zdero@URED.UDUOSK>
2018-03-23 15:17:03 +0100

    zadaca 8.5

---
+++ public/dzdero/funkcije.php
@@ -0,0 +1,14 @@
+<?php
+$filename='8.5.pisi.txt';
+$handle= fopen($filename,'w');
+$za_upis = $_GET["text"];
+fwrite($handle, $za_upis);
+fclose($handle);
+
+
+function pisi($text){
+$filename='8.5.pisi.txt';
+$handle= fopen($filename,'w');
+fwrite($handle, $text);
+fclose($handle);
+}
