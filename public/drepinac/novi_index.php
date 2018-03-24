1ad80656b3a9c2462eb75d4183c3f9b5d2de2920
drepinac <darko.repinac@udu-obz.hr>
2018-03-24 11:56:23 +0100

    Ponovo vježbanje ispita

---
+++ public/drepinac/novi_index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <?php
+        
+        echo '<pre>';
+        
+        print_r($data);
+        echo '</pre>';
+
+        ?>
+    </body>
+</html> 
