60df22bce5aa85af57bdd961400b9a922a5995af
drepinac <darko.repinac@udu-obz.hr>
2018-03-24 11:57:51 +0100

    rename stari file i dodavanje novog

--- public/drepinac/index.php
+++ public/drepinac/index.php
@@ -1,34 +1,14 @@
-<?php
-include 'data.php';
-$tabl = '<table border="1">';
-//$red = '<tr><td></td><td></td><td></td><td></td></tr>';
+<!DOCTYPE html>
+<html>
+    <body>
 
-echo '<table>';
-foreach ($data as $key => $value){
-    echo '<tr>';
-    echo '<td>'.$value['ime'],'</td>';
-    echo '<td>'.$value['prezime'],'</td>';
-    echo '<td>'.date_format(date_create($value['datum']),'d.m.Y').'</td>';
-    echo '<td>'.date_trans($value['datum']).'</td>';
-    echo '<td>'.'<select>';
-    if ($value['placeno'] == 'Da') {
-        echo '<option value="Da" Selected>Da</option>';
-        echo '<option value="Ne">Ne</option>';
-    } else {
-        echo '<option value="Ne" Selected>Ne</option>';
-        echo '<option value="Da">Da</option>';
-    }
-    echo '</select></td>';
-    
-    echo '</tr>';
-}
+        <?php
+        
+        echo '<pre>';
+        
+        print_r($data);
+        echo '</pre>';
 
-
-echo '</table>';
-
-function date_trans($datum){
-$tmp =  explode('-',$datum);
-    return '<td>'.$tmp[2].'.'.$tmp[1].'.'.$tmp[0].'</td>';
-}
-
-?>
+        ?>
+    </body>
+</html> 
