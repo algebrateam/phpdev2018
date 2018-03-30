0ae131a6db43eef4134ea6d3ebb0ddf63dac2c7a
drepinac <darko.repinac@udu-obz.hr>
2018-03-30 19:33:33 +0200

    Radovi u tijeku do 3.8

--- public/drepinac/mysql/Zadaca 3.4.sql
+++ public/drepinac/mysql/Zadaca 3.4.sql
@@ -35,3 +35,25 @@ where (nastavnik.koef+0.4)*800 < 3500 or (nastavnik.koef+0.4)*800 > 8000
 order by (nastavnik.koef+0.4)*800;
 
 -- Zadatak 3.7
+-- Ispišite imena i prezimena studenata koji su barem jednom pali na ispitu iz predmeta sa šiframa od 220 do 240;
+
+
+select stud.imeStud, stud.prezStud from stud
+inner join (
+            select ispit.mbrStud, ispit.sifPred, ispit.ocjena from ispit
+             where ispit.sifPred between 220 and 240
+               and ispit.ocjena = 1
+             order by ispit.mbrStud) ispiti 
+				                             on stud.mbrStud=ispiti.mbrStud;
+				                             
+-- Zadatak 3.8
+-- Ispiši imena i prezimena studenata koji su na nekom ispitu dobili 3.
+
+select mbrStud, stud.imeStud, stud.prezStud from stud
+inner join (
+            select ispit.mbrStud, ispit.sifPred, ispit.ocjena from ispit
+             where ispit.sifPred between 220 and 240
+               and ispit.ocjena = 1
+             order by ispit.mbrStud) ispiti 
+				                             on stud.mbrStud=ispiti.mbrStud;
+			                             
