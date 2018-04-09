e7e5690532cb79137cefa38d5e71907de8098247
BouliOS <danijel.krizanovic.com>
2018-04-09 15:20:10 +0200

    napravljena baza zadaca.5.6

--- public/dkrizanovic/Mysql/zadaca.5.6.sql
+++ public/dkrizanovic/Mysql/zadaca.5.6.sql
@@ -54,4 +54,5 @@ JOIN ispit ON ispit.sifNastavnik=nastavnik.sifNastavnik
 JOIN mjesto m1 ON m1.pbr=nastavnik.pbrStan
 JOIN stud ON stud.mbrStud=ispit.mbrStud
 JOIN mjesto m2 ON m2.pbr=stud.pbrRod
-WHERE MONTH(datIspit)=6 AND m1.nazMjesto=m2.nazMjesto;        
+WHERE MONTH(datIspit)=6 AND m1.nazMjesto=m2.nazMjesto;  
+      
