1896bde05bf8711ccdce5b497b656ce2e82e86fa
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-04-16 18:38:31 +0000

    Apply fixes from StyleCI

--- public/drepinac/web/skripta.php
+++ public/drepinac/web/skripta.php
@@ -6,12 +6,12 @@
     </head>
     <body>
         <?php
-            for ($i=1; $i<=5; $i++){
+            for ($i=1; $i<=5; $i++) {
                 echo '<a href="skripta.php?broj='.$i.'">'.$i.'</a><br />';
             }
         // put your code here
         if (isset($_GET['broj'])) {
-        echo 'Odabrali ste broj: '.$_GET['broj'];
+            echo 'Odabrali ste broj: '.$_GET['broj'];
         }
         ?>
     </body>
