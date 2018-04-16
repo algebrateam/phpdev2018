a7835820fdb202b371b765678ef11e87cde660a6
dkrizanovic <danijel.krizanovic@gmail.com>
2018-04-16 19:15:45 +0200

    napravljen folder web2

---
+++ public/dkrizanovic/web/1.5.Obrada_poslanih_datoteka.php
@@ -0,0 +1,5 @@
+<form method="POST" action="" enctype="multipart/form-data">
+        Datoteka za upload:<br/>
+    <input type="file" name="datoteka" value="" /><br/>
+        <input type="submit" name="upload_btn" value="Upload"/>
+        </form>
