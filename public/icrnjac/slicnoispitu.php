5b7a38894b4228f17811beb8798c991062420007
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-04-16 18:20:06 +0000

    Apply fixes from StyleCI

--- public/icrnjac/slicnoispitu.php
+++ public/icrnjac/slicnoispitu.php
@@ -35,9 +35,8 @@
 
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
