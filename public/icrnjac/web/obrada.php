39ec03b926adefe9384e430e8c373a8213042cc6
ivana871 <misscrnjac@gmail.com>
2018-04-16 20:05:14 +0200

    upload

--- public/icrnjac/web/obrada.php
+++ public/icrnjac/web/obrada.php
@@ -1,10 +1,11 @@
-<?php
+
 <form method="POST"action=""enctype="multipart/form-data">
         Datoteka za upload:<br/>
     <input type="file" name="datoteka"value=""/><br/>
     <input type="submit"name="upload_btn"value="Upload"/>
         </form>
- 
+ <?php
+ /*
         Array(
             [datoteka]=> Array (
                 [name]=>potvrda.pdf
@@ -14,18 +15,22 @@
                 [size]=>40909
                 )
             )
-        $uploaddir=basename(_DIR_)
+  * 
+  * 
+  * 
+  */
+  $uploaddir=basename(__DIR__);
 $uploadfile=basename($_FILES['datoteka']['name']);
 
 $file_array=explode(".",$uploadfile);
 $file_ext=end($file_array);
 $file_onserver="file_".time().".".$file_ext;
 $new_file_name=$uploaddir.$file_onserver;
-if(move_uploaded_file($_FILES['datoteka']['tmp_name'], ˘new_file_name´´
+if(move_uploaded_file($_FILES['datoteka']['tmp_name'], $new_file_name))
 {//blok koda
 }
-}
-     )
+
+  
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
