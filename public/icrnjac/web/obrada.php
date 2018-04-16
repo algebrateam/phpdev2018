106a5993fe79b0ae8a5795f97c1ef70a10757ee8
ivana871 <ivana.crnjac4@gmail.com>
2018-04-16 19:42:42 +0200

    primjer

---
+++ public/icrnjac/web/obrada.php
@@ -0,0 +1,35 @@
+<?php
+<form method="POST"action=""enctype="multipart/form-data">
+        Datoteka za upload:<br/>
+    <input type="file" name="datoteka"value=""/><br/>
+    <input type="submit"name="upload_btn"value="Upload"/>
+        </form>
+ 
+        Array(
+            [datoteka]=> Array (
+                [name]=>potvrda.pdf
+                [type]=>application/x-download
+                [tmp_name]=> C:\xampplite\tmp\php3EBE.tmp
+                [error]=>0
+                [size]=>40909
+                )
+            )
+        $uploaddir=basename(_DIR_)
+$uploadfile=basename($_FILES['datoteka']['name']);
+
+$file_array=explode(".",$uploadfile);
+$file_ext=end($file_array);
+$file_onserver="file_".time().".".$file_ext;
+$new_file_name=$uploaddir.$file_onserver;
+if(move_uploaded_file($_FILES['datoteka']['tmp_name'], ˘new_file_name´´
+{//blok koda
+}
+}
+     )
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
