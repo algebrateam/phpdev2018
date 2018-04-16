e3bc38bda1f93081cab90e2e1f1bc4f451e8ac44
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-04-16 17:38:48 +0000

    Apply fixes from StyleCI

--- public/drepinac/web/primjer_1.php
+++ public/drepinac/web/primjer_1.php
@@ -18,16 +18,14 @@
         $uploaddir = basename(__DIR__);
         $uploadfile = basename($_FILES['datoteka']['name']);
         
-        $file_array = explode(".",$uploadfile);
+        $file_array = explode(".", $uploadfile);
         $file_ext = end($file_array);
         
         $file_onserver = "file_".time().".".$file_ext;
         
         $new_file_name = $uploaddir.$file_onserver;
         
-        if (move_uploaded_file($_FILES['datoteka']['tmp_name'],$new_file_name))
-        {
-            
+        if (move_uploaded_file($_FILES['datoteka']['tmp_name'], $new_file_name)) {
         }
         // put your code here
         ?>
