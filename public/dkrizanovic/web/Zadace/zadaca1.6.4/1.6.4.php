c8643fe1258634ab91808c3dfc464fa08794613b
BouliOS <danijel.krizanovic.com>
2018-04-17 23:30:17 +0200

    napravljen fajl upload.php za drugi dio zadace

--- public/dkrizanovic/web/Zadace/zadaca1.6.4/1.6.4.php
+++ public/dkrizanovic/web/Zadace/zadaca1.6.4/1.6.4.php
@@ -1,8 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <body>
+        <div>
+            <form method="GET" action="nastavak.php" method="post" enctype="multipart/form-data">
+                Odaberite broj datoteka za upload!
+                                    <br>
+                                    <br>
+                <select name="brojfajlova">
+                    <option value="">Izaberite...</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <br/>
+                <br/>    
+                <input type="submit" value="Nastavak" name="submit"> <p></p>
+                <p>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+                </p>
+            </form>
+        </div>
+    </body>
+</html>
