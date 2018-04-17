fd3f5e026f60c18df5bfa779f1f2278f1b5bf721
belasken <bela.skenderovic@hdit.hr>
2018-04-17 10:40:07 +0200

    zadaca 1.6.4

---
+++ public/bskenderovic/Zadaca/zadaca1.6.4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>1.6.4</title>
+</head>
+<body>
+
+    <form action="zadaca1.6.4.datoteka.php" method="GET">
+	<p>Broj datoteka za upload</p>
+	<input type="number" type="number" name="filebr" min="1" max="5">
+	<button type="submit" name="submit_btn">Nastavak</button>
+</form>
+
+</body>
+
+</html>
