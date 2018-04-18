4975aba9dce4ec142bac666dfd132816aae00456
dkrizanovic <danijel.krizanovic@gmail.com>
2018-04-18 19:24:40 +0200

    napravljen folder za danasnje predavanje

---
+++ public/dkrizanovic/web/18.04/spojnabazu.php
@@ -0,0 +1,19 @@
+<?php
+$host='localhost';
+$user='root';
+$password='';
+$database='fakultet';
+$mysqli=mysqli_connect($host, $user, $password, $database);
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>Spoj na bazu pomoću mysqli</div>
+    </body>
+</html>
