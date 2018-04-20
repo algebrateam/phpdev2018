378ab560324f747878b273105833ac7bdb4b6584
BouliOS <danijel.krizanovic.com>
2018-04-20 12:42:36 +0200

    ispravljen utf8

--- public/dkrizanovic/web/Zadace/zadaca3.10/db_connection.php
+++ public/dkrizanovic/web/Zadace/zadaca3.10/db_connection.php
@@ -1,12 +1,14 @@
 <?php
-$host='localhost';
-$user='root';
-$password='';
-$database='fakultet';
-$link=mysqli_connect($host, $user, $password, $database);
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'fakultet';
+$link = mysqli_connect($host, $user, $password, $database);
 mysqli_set_charset($link, 'utf8');
-if($link){}
-else {
+if ($link) {
+    
+} else {
     die('Neuspjela konekcija');
 }
     
