c911bb3bdbba04b3cdb3593ca5303a18f080d7c6
BouliOS <danijel.krizanovic.com>
2018-04-20 12:41:01 +0200

    napravljen zadatak 3.10

--- public/dkrizanovic/web/Zadace/zadaca3.10/db_connection.php
+++ public/dkrizanovic/web/Zadace/zadaca3.10/db_connection.php
@@ -4,7 +4,10 @@ $user='root';
 $password='';
 $database='fakultet';
 $link=mysqli_connect($host, $user, $password, $database);
+mysqli_set_charset($link, 'utf8');
 if($link){}
 else {
     die('Neuspjela konekcija');
 }
+    
+
