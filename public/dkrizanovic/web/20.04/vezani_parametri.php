c3a8ed929116894ad0b5d1ac80f404bb0137a655
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-04-20 18:48:14 +0000

    Apply fixes from StyleCI

--- public/dkrizanovic/web/20.04/vezani_parametri.php
+++ public/dkrizanovic/web/20.04/vezani_parametri.php
@@ -1,15 +1,13 @@
 <?php
-$mysqli= new mysqli('localhost','root','','fakultet');
-if(mysqli_connect_errno())
-{
+$mysqli= new mysqli('localhost', 'root', '', 'fakultet');
+if (mysqli_connect_errno()) {
     echo 'Doslo je do pogreske:';
     echo mysqli_connect_error();
     exit();
 }
 $query_tpl="SELECT pbr FROM mjesto WHERE nazMjesto=?";
 $naziv="Zagreb";
-if($stmt=$mysqli->prepare($query_tpl))
-{
+if ($stmt=$mysqli->prepare($query_tpl)) {
     $stmt->bind_param('s', $naziv);
     $stmt->execute();
     $stmt->bind_result($pbr);
@@ -18,5 +16,3 @@ if($stmt=$mysqli->prepare($query_tpl))
     $stmt->close();
 }
 $mysqli->close();
-
-
