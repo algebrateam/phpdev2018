498adcdc4774b3e250975df014c374619d95c8d1
domagoj-zdero <domagoj.zdero@gmail.com>
2018-05-02 21:29:08 +0200

    web4 sa nastave 02.05.2018.

--- public/dzdero/web4/dbconn.php
+++ public/dzdero/web4/dbconn.php
@@ -1,14 +1,12 @@
 <?php
-$host='localhost'; //127.0.0.1
+$host='localhost';  // 127.0.0.1
 $user='root';
 $password='';
 $database='fakultet';
-$mysqli=new mysqli($host, $user, $password, $database);
-//mysql_set_charset($link, 'utf8');
+$mysqli= new mysqli($host, $user, $password, $database);
+//mysqli_set_charset($link, 'utf8');
 if ($mysqli) {
-    echo 'Uspjesno povezana baza :-)'.'<br>';
 } else {
-   // echo mysqli_connect_error();
-    die('Neuspjela konekcija');
+    // echo mysqli_connect_error();
+    die('Neuspjela konekcija!');
 }
-?>
