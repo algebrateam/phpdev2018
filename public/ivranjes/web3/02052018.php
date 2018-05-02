dd2980913da173b2cf1439166874802837cc1e66
ivranjes <ivan.vranjes@t.ht.hr>
2018-05-02 21:30:23 +0200

    new4

---
+++ public/ivranjes/web3/02052018.php
@@ -0,0 +1,47 @@
+<?php
+$host='localhost';  // 127.0.0.1
+$user='root';
+$password='';
+$database='fakultet';
+$mysqli= new mysqli($host, $user, $password, $database);
+//mysqli_set_charset($link, 'utf8');
+if ($mysqli) {
+} else {
+    // echo mysqli_connect_error();
+    die('Neuspjela konekcija!');
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <div>
+      <?php
+      $query ="SELECT * FROM stud LIMIT 50";
+      $result= $mysqli->query($query);
+      while ($row = $result->fetch_assoc()) {
+          ?>
+        <table>
+            <thead>
+            <tr><td></td><td></td></th>
+            </thead>
+            <tbody>
+        </table>
+              
+          echo ." ". $row['imeStud']." ".$row['prezStud'];
+      }
+      echo "<hr>";
+      echo $mysqli->affected_rows;
+       echo $mysqli->server_info;
+    ?>
+    
+    </div>
+  </body>
+</html>
+<?php $mysqli->close(); ?>
+
---
+++ public/ivranjes/web3/mysqli_objektni_primjer.php
@@ -0,0 +1,27 @@
+<?php
+include_once 'db_connection.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Proceduralni objekti mysqli</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>
+            <?php
+            $query = "SELECT * FROM stud LIMIT 50";
+            $result = $mysqli->query($query);
+            while ($row = $result->fetch_assoc()) {
+                echo " " . $row['imeStud'] . " " . $row['prezStud'];
+            }
+
+            echo "<hr>";
+            echo $mysqli->afected_rows;
+            ?>
+
+        </div>
+    </body>
+</html>
+<?php $mysqli->close(); ?>
