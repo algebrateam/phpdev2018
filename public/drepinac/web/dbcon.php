6be3bb9ddf8c6e4f001b255e0ce0075a80aecd6c
drepinac <darko.repinac@udu-obz.hr>
2018-05-02 21:30:11 +0200

    novo

--- public/drepinac/web/dbcon.php
+++ public/drepinac/web/dbcon.php
@@ -4,6 +4,7 @@ $user='root';
 $password='';
 $database='fakultet';
 $link=mysqli_connect($host, $user, $password, $database);
+mysqli_set_charset($link, 'utf8');
 if ($link) {
 } else {
     echo mysqli_connect_error();
