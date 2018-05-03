951e33606352cfdb5724084afe71e4e8e909848d
mrvic <mrvic.predrag@gmail.com>
2018-05-03 22:08:59 +0200

    promjena dmconn set_charset utf-8

--- public/pmrvic/web4/dbconn.php
+++ public/pmrvic/web4/dbconn.php
@@ -4,7 +4,7 @@ $user='root';
 $password='';
 $database='fakultet';
 $mysqli= new mysqli($host, $user, $password, $database);
-//mysqli_set_charset($link, 'utf8');
+$mysqli->set_charset("utf8");
 if ($mysqli) {
 } else {
     // echo mysqli_connect_error();
