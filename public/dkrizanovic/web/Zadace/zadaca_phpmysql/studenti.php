0ee80ed4e55348c0583546ac8a6b8d0f4b11c667
BouliOS <danijel.krizanovic.com>
2018-05-03 13:37:44 +0200

    napravljen meni za biranje studenata

---
+++ public/dkrizanovic/web/Zadace/zadaca_phpmysql/studenti.php
@@ -0,0 +1,29 @@
+<?php
+include './db_connection.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Studenti</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>
+              <form name="form_update" method="post" action="testiranje.php">
+<?php
+echo "<select name= 'Student'>";
+echo '<option value="">'.'--- Izaberi Studenta ---'.'</option>';
+$query = mysqli_query($mysqli,"SELECT * FROM stud ORDER BY imeStud ASC LIMIT 15;");
+while($row=mysqli_fetch_array($query))
+{
+    echo "<option value='". $row['mbrStud']."'>".$row['imeStud']." ".$row['prezStud']
+ .'</option>';
+}
+echo '</select>';
+?> <input type="submit" name="submit" value="Provjeri"/>
+</form>
+<br/><br/>
+        </div>
+    </body>
+</html>
