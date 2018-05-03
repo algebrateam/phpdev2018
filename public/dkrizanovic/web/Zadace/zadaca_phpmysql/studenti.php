d691e41164b153248df7ff9f829dc15c81497e6f
Predrag Mrvic <mrvic.predrag@gmail.com>
2018-05-03 19:45:01 +0000

    Apply fixes from StyleCI

--- public/dkrizanovic/web/Zadace/zadaca_phpmysql/studenti.php
+++ public/dkrizanovic/web/Zadace/zadaca_phpmysql/studenti.php
@@ -14,9 +14,8 @@ include_once './db_connection.php';
 <?php
 echo "<select name= 'Student'>";
 echo '<option value="">'.'--- Izaberi Studenta ---'.'</option>';
-$query = mysqli_query($mysqli,"SELECT * FROM stud ORDER BY imeStud ASC LIMIT 15;");
-while($row=mysqli_fetch_array($query))
-{
+$query = mysqli_query($mysqli, "SELECT * FROM stud ORDER BY imeStud ASC LIMIT 15;");
+while ($row=mysqli_fetch_array($query)) {
     echo "<option value='". $row['mbrStud']."'>".$row['imeStud']." ".$row['prezStud']
  .'</option>';
 }
