200d1480a53c70c14ea5d014c0200ad13ebcdc40
ivana871 <misscrnjac@gmail.com>
2018-05-18 19:35:56 +0200

    ispit

---
+++ public/dperic/ispit/dbconn.php
@@ -0,0 +1,13 @@
+<?php
+$host='localhost';
+$user='root';
+$password='';
+
+$database='fakultet';
+$mysqli = new mysqli($host, $user, $password, $database);
+
+
+if ($mysqli) {
+} else {
+    die('Neuspjela konekcija');
+}
